a61390bef94ce5fc5a1dc74e576f7170f3dd911d
Matheus Ochi <matheus.ochi@ciadaconsulta.com.br>
2020-08-30 18:26:53 -0300

    Creates my first program on java

---
+++ first-program/Exercicio1.java
@@ -0,0 +1,7 @@
+class Exercicio1 {
+    public static void main(String[] args) {
+        System.out.println("Essa é a primeira linha da minha aplicação");
+        System.out.println("Essa é outra linha da minha aplicação!");
+        System.out.println("Esse é um println \ncom duas linhas!");
+    }
+}
