a5860291f0ee7a6d7dd2673fc35607a70a68b64d
Matheus Ochi <matheus.ochi@ciadaconsulta.com.br>
2020-08-30 18:37:57 -0300

    Creates primitives-variables folder, which contain my study of Java primitive variables

---
+++ primitive-variables/TestaIdade.java
@@ -0,0 +1,13 @@
+class TestaIdade {
+    public static void main(String[] args) {
+        int idade = 20;
+        String nome = "Matheus";
+        System.out.println(idade);
+        System.out.println(nome);
+
+        int idadeAnoQueVem;
+        idadeAnoQueVem = idade + 1;
+
+        System.out.println(idadeAnoQueVem);
+    }
+}
